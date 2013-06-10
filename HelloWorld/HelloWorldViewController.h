82c90009a418787ea82079f6b6605b726866913e
Srinivasan G <srinivasan@mozzapp.in>
2013-06-10 18:45:04 +0530

    HelloWorld App
    Sample Creation

---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Mozzapp on 10/06/13.
+//  Copyright (c) 2013 Mozzapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
---
+++ HelloWorld/HelloWorldViewController.m
@@ -0,0 +1,29 @@
+//
+//  HelloWorldViewController.m
+//  HelloWorld
+//
+//  Created by Mozzapp on 10/06/13.
+//  Copyright (c) 2013 Mozzapp. All rights reserved.
+//
+
+#import "HelloWorldViewController.h"
+
+@interface HelloWorldViewController ()
+
+@end
+
+@implementation HelloWorldViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
